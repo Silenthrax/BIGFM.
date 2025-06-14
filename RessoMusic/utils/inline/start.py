03bedbd8b8a640fce0ad0a6b869a7549e6fd8b0f
Silenthrax <210396117+Silenthrax@users.noreply.github.com>
2025-06-14 20:37:58 +0530

    Update start.py

--- RessoMusic/utils/inline/start.py
+++ RessoMusic/utils/inline/start.py
@@ -19,6 +19,7 @@ def start_panel(_):
 def private_panel(_):
     buttons = [
         [
+            InlineKeyboardButton(text=_[""], user_id=config.OWNER_ID)
             InlineKeyboardButton(
                 text=_["S_B_3"],
                 url=f"https://t.me/{app.username}?startgroup=true",
@@ -26,7 +27,7 @@ def private_panel(_):
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.SUPPORT_CHAT)
+            InlineKeyboardButton(text=_["S_B_9"], user_id=config.SUPPORT_CHAT)
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
         ],
         [
@@ -35,3 +36,4 @@ def private_panel(_):
         ],
     ]
     return buttons
+
